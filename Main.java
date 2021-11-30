9adfeb7ad050fc83b4ee919cbfdc26570c5703c3
Jeff3003 <75411602+Jeff3003@users.noreply.github.com>
2021-11-30 00:41:47 -0300

    Teste

---
+++ Main.java
@@ -0,0 +1,16 @@
+package ifs;
+
+public class IF {
+
+    public static void main(String[] args) {
+        final var condicao = false;
+
+        if (condicao) {
+            System.out.println( " a cond");
+        }else {
+            System.out.println("A cond eh");
+        }
+        System.out.println(condicao);
+	// write your code here
+    }
+}
